7c6893b5a9b009ae56a6a8b1c2bb333599e21c64
brijeshkumar <kumarbrijesh25729@gmail.com>
2023-10-15 23:40:43 +0530

    Changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -54,9 +54,6 @@
                             <i class="fas fa-user-cog mr-2"></i> Settings
                         </a>
                         <div class="dropdown-divider"></div>
-                        <a href="#" class="dropdown-item">
-                            <i class="fas fa-lock mr-2"></i> Change Password
-                        </a>
                         <div class="dropdown-divider"></div>
                         <a href="{{route('user.logout')}}" class="dropdown-item text-danger">
                             <i class="fas fa-sign-out-alt mr-2"></i> Logout
